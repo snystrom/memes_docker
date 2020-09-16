891db99a411ff9c4b08479f6d7dcf273fe35e53f
snystrom <snystrom@users.noreply.github.com>
2020-09-16 12:03:20 -0400

    force install from bioc dev branch 3.12.

--- install_deps.R
+++ install_deps.R
@@ -1,4 +1,6 @@
 remotes::install_github("snystrom/cmdfun")
 remotes::install_cran("BiocManager")
+#BiocManager::install(version='devel')
+Sys.setenv("R_BIOC_VERSION" = "3.12")
 remotes::install_github("snystrom/memes", dependencies = TRUE)
 
